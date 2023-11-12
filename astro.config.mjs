ba32d40971a8924cc360fd6af6427c6e2f697ce8
236sec <popole159za@gmail.com>
2023-11-12 19:59:17 +0700

    Add vercel fix Dynamic Blog Page route

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind(), react()],
   markdown: {
     syntaxHighlight: 'prism'
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
