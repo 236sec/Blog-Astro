ebb9846f0e844783ee7b429aa86cafbbaebdedeb
236sec <popole159za@gmail.com>
2024-02-03 16:03:41 +0700

    Add Mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import rehypeMermaid from 'rehype-mermaid'
+import rehypeShikiji from 'rehype-shikiji'
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind(), react()],
   markdown: {
-    syntaxHighlight: 'prism'
+    //syntaxHighlight: 'prism',
+    syntaxHighlight: false,
+    rehypePlugins: [rehypeMermaid, [rehypeShikiji, { theme: 'github-dark' }]],
   },
   output: "server",
   adapter: vercel()
--- src/content/blogs/post-3.md
+++ src/content/blogs/post-3.md
@@ -9,3 +9,12 @@ pubDate: 2022-07-15
 tags: ["astro", "learning in public", "setbacks", "community"]
 ---
 It wasn't always smooth sailing, but I'm enjoying building with Astro. And, the [Discord community](https://astro.build/chat) is really friendly and helpful!
+
+```mermaid
+---
+title: การเช่าหนัง
+---
+flowchart LR
+    id1(((start))) --> id2
+    id3[(Database)] --> id2[This is the text in the box]
+```
