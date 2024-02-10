43da503b5770fb7cebbdee037f8c20d0b010fe68
236sec <popole159za@gmail.com>
2024-02-10 21:05:49 +0700

    Remove mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import rehypeMermaid from 'rehype-mermaid'
-import rehypeShikiji from 'rehype-shikiji'
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind(), react()],
   markdown: {
-    //syntaxHighlight: 'prism',
-    syntaxHighlight: false,
-    rehypePlugins: [rehypeMermaid, [rehypeShikiji, { theme: 'github-dark' }]],
+    syntaxHighlight: 'prism',
   },
   output: "server",
   adapter: vercel()
